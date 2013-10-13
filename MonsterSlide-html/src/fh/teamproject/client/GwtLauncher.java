f5ead2c2099581a7917c1cc0a94be3791e778e24
torsten <hotteport@online.de>
2013-10-13 20:38:30 +0200

    Initial project setup inklusive gitignore

---
+++ MonsterSlide-html/src/fh/teamproject/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package fh.teamproject.client;
+
+import fh.teamproject.MonsterSlide;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new MonsterSlide();
+	}
+}
