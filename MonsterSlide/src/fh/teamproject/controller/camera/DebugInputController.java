847696346d6f1f0c2b6aa2bd735db761228454ca
torsten <hotteport@online.de>
2013-11-22 16:22:13 +0100

    Richte DebugInputController ein

---
+++ MonsterSlide/src/fh/teamproject/controller/camera/DebugInputController.java
@@ -0,0 +1,30 @@
+package fh.teamproject.controller.camera;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+
+import fh.teamproject.screens.GameScreen;
+import fh.teamproject.utils.CameraManager.Mode;
+
+public class DebugInputController extends InputAdapter {
+
+	GameScreen gameScreen;
+
+	public DebugInputController(GameScreen gameScreen) {
+		this.gameScreen = gameScreen;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		switch (keycode) {
+		case Keys.NUM_1:
+			this.gameScreen.camManager.setMode(Mode.CHASE);
+			return true;
+		case Keys.NUM_2:
+			this.gameScreen.camManager.setMode(Mode.FREE);
+			return true;
+		}
+
+		return false;
+	}
+}
