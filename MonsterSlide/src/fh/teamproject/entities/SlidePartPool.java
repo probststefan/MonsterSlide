d96f59d3fc92808e6824cd452193f14ac62f7ea0
Stefan Probst <probststefan@gmail.com>
2013-11-27 15:40:41 +0100

    Slide und SlidePart wurden komplett neu geschrieben. Sie werden jetzt immer vom Nullpunkt translatiert und auch immer wieder wiederverwendet. Es gibt aber auch noch einen kleinen Bug: Nach einer gewissen Zeit werden die SlideParts nicht mehr gerendert. - Jetzt mit den fehlenden 2 Dateien.

---
+++ MonsterSlide/src/fh/teamproject/entities/SlidePartPool.java
@@ -0,0 +1,16 @@
+package fh.teamproject.entities;
+
+import com.badlogic.gdx.utils.Pool;
+
+import fh.teamproject.interfaces.ISlidePart;
+
+public class SlidePartPool extends Pool<ISlidePart> {
+
+	@Override
+	protected ISlidePart newObject() {
+		SlidePart slidePart = new SlidePart();
+		slidePart.createSlidePart();
+		return slidePart;
+	}
+
+}
