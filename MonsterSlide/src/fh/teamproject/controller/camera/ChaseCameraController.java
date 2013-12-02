27bcce6c7a618e8a3565bce6ec9f2d45f6f3abd7
torsten <hotteport@online.de>
2013-12-02 18:42:29 +0100

    minor change

--- MonsterSlide/src/fh/teamproject/controller/camera/ChaseCameraController.java
+++ MonsterSlide/src/fh/teamproject/controller/camera/ChaseCameraController.java
@@ -1,6 +1,5 @@
 package fh.teamproject.controller.camera;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Vector3;
 
@@ -32,9 +31,9 @@ public class ChaseCameraController extends CameraController {
 		Vector3 pos = this.player.getPosition();
 		Vector3 orbitPos = pos.cpy().add(this.offset);
 		this.camera.position.set(orbitPos);
-		// this.camera.rotateAround(pos, rotAxis, 315);
-		this.camera.lookAt(pos);
-		Gdx.app.log("ChaseCam", "Pos" + this.camera.up);
+		this.camera.rotateAround(pos, rotAxis, 315);
+		this.camera.lookAt(pos.cpy().add(this.player.direction));
+		// Gdx.app.log("ChaseCam", "Pos" + this.camera.up);
 		// Gdx.app.log("ChaseCam", "Orbit" + orbitPos);
 
 		super.update();
