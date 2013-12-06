2c155097a52bed395a052410a8f32bf2a3f345f1
Evenrider <kkev5621@gmail.com>
2013-12-06 13:26:35 +0100

    Font added

--- MonsterSlide/src/fh/teamproject/screens/menusites/DemoSite.java
+++ MonsterSlide/src/fh/teamproject/screens/menusites/DemoSite.java
@@ -1,8 +1,11 @@
 package fh.teamproject.screens.menusites;
 
+import org.omg.CORBA.FREE_MEM;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.esotericsoftware.tablelayout.BaseTableLayout.Debug;
@@ -27,16 +30,20 @@ public class DemoSite extends AbstractMenuSite {
         for (@SuppressWarnings("rawtypes") com.esotericsoftware.tablelayout.Cell c : this.getCells()) {
             c.size(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         }
-        //Styles
+        //Styles and Font
+        //Build the Fontgenerator, Generate the Font and dispose the Generator
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/Outage.ttf"));
+        BitmapFont font = generator.generateFont(12 + Gdx.graphics.getHeight()/10); // font size 12 pixels + 1 for every 100 Pixel in the Height
+        generator.dispose(); // don't forget to dispose to avoid memory leaks!
+        //Create a Style with the Colors and add fhe font to it
         style = new TextButtonStyle();
         style.checkedFontColor = new Color(Color.BLUE);
         style.checkedOverFontColor = new Color(Color.MAGENTA);
         style.downFontColor = new Color(Color.GREEN);
         style.fontColor = new Color(Color.BLUE);
         style.overFontColor = new Color(Color.MAGENTA);
-        style.font = new BitmapFont();
-        style.font.scale(2.0f);
-        //Listener + TextButton
+        style.font = font;
+        //Listener and TextButton
         listener = new ButtonListener("Game",menu);
         start = new TextButton("New Game", style);
         start.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -61,5 +68,6 @@ public class DemoSite extends AbstractMenuSite {
         //System.out.println("TableBreite: " + this.getWidth() + "    TableHöhe: " + this.getHeight());
         //System.out.println("ButtonBreite: " + start.getWidth() + "    ButtonHöhe: " + start.getHeight());
         //System.out.println("CellBreite: " + width + "    CellHöhe: " + height);
+        start.center();
     }
 }
