8094fe65a6c124f2b70cd16d832fafd90b93f26a
Evenrider <kkev5621@gmail.com>
2013-12-06 13:56:52 +0100

    Label has to be clicked, Label now allways in the middle

--- MonsterSlide/src/fh/teamproject/screens/menusites/DemoSite.java
+++ MonsterSlide/src/fh/teamproject/screens/menusites/DemoSite.java
@@ -1,7 +1,5 @@
 package fh.teamproject.screens.menusites;
 
-import org.omg.CORBA.FREE_MEM;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -9,7 +7,6 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.esotericsoftware.tablelayout.BaseTableLayout.Debug;
-import com.esotericsoftware.tablelayout.Cell;
 
 import fh.teamproject.screens.MenuScreen;
 import fh.teamproject.screens.utils.ButtonListener;
@@ -22,18 +19,16 @@ public class DemoSite extends AbstractMenuSite {
     
     public DemoSite(MenuScreen menu)
     {
-        //Tables
         super();
+        //set the Table
         //this.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/mainmenu.jpg"), true))));
-        this.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        this.center();
-        for (@SuppressWarnings("rawtypes") com.esotericsoftware.tablelayout.Cell c : this.getCells()) {
-            c.size(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        }
+        this.setWidth(Gdx.graphics.getWidth());
+        this.setHeight(Gdx.graphics.getHeight());
+        //this.center();
         //Styles and Font
         //Build the Fontgenerator, Generate the Font and dispose the Generator
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/Outage.ttf"));
-        BitmapFont font = generator.generateFont(12 + Gdx.graphics.getHeight()/10); // font size 12 pixels + 1 for every 100 Pixel in the Height
+        BitmapFont font = generator.generateFont((int) (12 + Gdx.graphics.getHeight()/10)); // font size 12 pixels + 1 for every 100 Pixel in the Gdx.graphics.getHeight()
         generator.dispose(); // don't forget to dispose to avoid memory leaks!
         //Create a Style with the Colors and add fhe font to it
         style = new TextButtonStyle();
@@ -46,28 +41,21 @@ public class DemoSite extends AbstractMenuSite {
         //Listener and TextButton
         listener = new ButtonListener("Game",menu);
         start = new TextButton("New Game", style);
-        start.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        start.setWidth(Gdx.graphics.getWidth());
+        start.setHeight(Gdx.graphics.getHeight());
         start.addListener(listener);
-        start.center();
+        //start.center();
         //Adden Rückwärts vom erstellen
-        this.add(start);
+        this.add(start).expandX().expandY();
         this.debug(Debug.all);
+        // TODO Auto-generated method stub
+        //System.out.println("ResizeBreite: " + width + "    ResizeHöhe: " + Gdx.graphics.getHeight());
+        //System.out.println("TableBreite: " + this.getWidth() + "    TableHöhe: " + this.getGdx.graphics.getHeight()());
+        //System.out.println("ButtonBreite: " + start.getWidth() + "    ButtonHöhe: " + start.getGdx.graphics.getHeight()());
     }
 
     @Override
     public void resize(float width, float height) {
-        // TODO Auto-generated method stub
-        this.setSize(width, height);
-        //float w = 0;
-        //float h = 0;
-        //System.out.println("Breite: " + width + "    Höhe: " + height);
-        for (@SuppressWarnings("rawtypes") Cell c : this.getCells()) {
-            c.size(width, height);
-        }
-        start.setSize(width, height);
-        //System.out.println("TableBreite: " + this.getWidth() + "    TableHöhe: " + this.getHeight());
-        //System.out.println("ButtonBreite: " + start.getWidth() + "    ButtonHöhe: " + start.getHeight());
-        //System.out.println("CellBreite: " + width + "    CellHöhe: " + height);
-        start.center();
+
     }
 }
