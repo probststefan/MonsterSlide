bda554cd1b7596cb01d03f45635367d39e752d2e
torsten <hotteport@online.de>
2014-02-18 14:21:35 +0100

    menuscreen in richtiges package verschoben

--- MonsterSlide/src/fh/teamproject/screens/menusites/AbstractMenuSite.java
+++
@@ -1,7 +0,0 @@
-package fh.teamproject.screens.menusites;
-
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-
-public abstract class AbstractMenuSite extends Table {
-    public abstract void resize(float width, float height);
-}
